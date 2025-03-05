fe8f1fcc34f5b824198823eb009a7b1270fe0975
Sahil Goel <sahilgoel@Mac.lan>
2025-03-05 09:49:48 -0800

    Implemented walking anim

--- Assets/Scripts/2DMovement/PlayerMovement.cs
+++ Assets/Scripts/2DMovement/PlayerMovement.cs
@@ -6,12 +6,20 @@ public class PlayerMovement : MonoBehaviour
     public float moveSpeed = 4f;
     public float sprintMultiplier = 1.5f;
     public LayerMask obstacleLayers;
-    public Vector2 collisionCheckSize = new Vector2(0.8f, 0.8f);
     public AudioClip blockedSound;
     public float blockedSoundVolume = 1f;
     public float blockedSoundCadence = 0.5f;
     private float lastBlockedSoundTime = -Mathf.Infinity;
     private bool isMoving = false;
+    public Animator animator;
+    public SpriteRenderer spriteRenderer;
+    private BoxCollider2D boxCollider;
+    public Vector2 lastMoveDirection;
+
+    void Awake()
+    {
+        boxCollider = GetComponent<BoxCollider2D>();
+    }
 
     void Update()
     {
@@ -22,6 +30,7 @@ public class PlayerMovement : MonoBehaviour
                 input.y = 0;
             if (input != Vector2.zero)
             {
+                lastMoveDirection = input;
                 StartCoroutine(Move(input));
             }
         }
@@ -32,11 +41,31 @@ public class PlayerMovement : MonoBehaviour
         isMoving = true;
         float currentSpeed = moveSpeed;
         if (Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.RightShift))
-        {
             currentSpeed *= sprintMultiplier;
-        }
         Vector3 startPos = transform.position;
         Vector3 targetPos = startPos + new Vector3(direction.x, direction.y, 0);
+        
+        if (direction.y > 0)
+        {
+            animator.SetTrigger("WalkUp");
+            spriteRenderer.flipX = false;
+        }
+        else if (direction.y < 0)
+        {
+            animator.SetTrigger("WalkDown");
+            spriteRenderer.flipX = false;
+        }
+        else if (direction.x < 0)
+        {
+            animator.SetTrigger("WalkLeft");
+            spriteRenderer.flipX = false;
+        }
+        else if (direction.x > 0)
+        {
+            animator.SetTrigger("WalkLeft");
+            spriteRenderer.flipX = true;
+        }
+        
         if (IsBlocked(targetPos))
         {
             if (blockedSound != null && Time.time - lastBlockedSoundTime >= blockedSoundCadence)
@@ -47,6 +76,7 @@ public class PlayerMovement : MonoBehaviour
             isMoving = false;
             yield break;
         }
+
         float timeToMove = 1f / currentSpeed;
         float elapsedTime = 0f;
         while (elapsedTime < timeToMove)
@@ -61,13 +91,15 @@ public class PlayerMovement : MonoBehaviour
 
     bool IsBlocked(Vector3 targetPos)
     {
-        Collider2D hit = Physics2D.OverlapBox(targetPos, collisionCheckSize, 0f, obstacleLayers);
-        return (hit != null);
-    }
-
-    void OnDrawGizmos()
-    {
-        Gizmos.color = Color.red;
-        Gizmos.DrawWireCube(transform.position, collisionCheckSize);
+        Vector2 size = boxCollider.bounds.size;
+        Collider2D[] hits = Physics2D.OverlapBoxAll(targetPos, size, 0f, obstacleLayers);
+        foreach (Collider2D hit in hits)
+        {
+            if (hit.gameObject != gameObject)
+            {
+                return true;
+            }
+        }
+        return false;
     }
 }
