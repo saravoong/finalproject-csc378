40a9a68e2dccae6c23b01ee94c04eff8d226e411
WingsumSiuu <wsiu01@calpoly.edu>
2025-03-17 12:20:46 -0700

    ending scene

--- Assets/Assets/CurtainAssets/ShowButton.cs
+++ Assets/Assets/CurtainAssets/ShowButton.cs
@@ -1,17 +1,25 @@
 using UnityEngine;
 using UnityEngine.UI;
 using System.Collections;
+using TMPro;
 
 public class ShowButton : MonoBehaviour
 {
     private Button button;
     private Image buttonImage;
+    [SerializeField] private TextMeshProUGUI dialoguetext;
     void Start()
     {
         button = GetComponent<Button>();
         buttonImage = button.GetComponent<Image>();
-        Invoke("showButton", 4f); 
     }
+
+    void Update() {
+        if (dialoguetext.text.Contains("Congratulations Beetrice, you have officially become our Kingdom Baker!")) {
+            showButton();
+        }
+    }
+
     private void showButton() {
         Color color = buttonImage.color;
         color.a = 1f;
